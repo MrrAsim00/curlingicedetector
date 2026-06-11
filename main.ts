5808b8d39649d5063ccdf08f066b7e2d58e58ff5
asiimode <114370200+MrrAsim00@users.noreply.github.com>
2026-06-11 09:33:23 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    // Displays the current temperature on the LED grid
+    basic.showNumber(input.temperature())
+    basic.showString("C*")
+    basic.pause(2000)
+})
